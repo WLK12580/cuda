58cc1066598555096259aead99fd6e20e8731679
wlk <15938712580@163.com>
2024-03-20 22:29:25 +0800

    learnCuda

--- cuda_array.cu
+++ cuda_array.cu
@@ -4,8 +4,8 @@
 
 #include <array>
 #include <iostream>
-__global__ void kernel_func(int *arr13, int *arr11, int *arr12) {
-  int idx = threadIdx.x;
+__global__ void kernel_func(int *arr13, int *arr11, int *arr12) {  //__global__ 表示核函数在GPU上执行
+  int idx = threadIdx.x;  //threadIdx指的是线程的索引
   arr13[idx] = arr11[idx] * arr12[idx];
 }
 int main() {
@@ -21,7 +21,7 @@ int main() {
   cudaMemcpy(arr11, arr1, sizeof(int) * 3, cudaMemcpyHostToDevice); //从主机复制数据到GPU
   cudaMemcpy(arr12, arr2, sizeof(int) * 3, cudaMemcpyHostToDevice);
   cudaMemcpy(arr13, arr3, sizeof(int) * 3, cudaMemcpyHostToDevice);
-  kernel_func<<<1, 3>>>(arr13, arr11, arr12);
+  kernel_func<<<1, 3>>>(arr13, arr11, arr12); //分配一个块三个线程，同一个块中的线程是同步的
 
   cudaError_t  cudaStatus = cudaMemcpy(arr3, arr13, 3 * sizeof(int), cudaMemcpyDeviceToHost); //将计算结果返回到主机
   if(cudaStatus != cudaSuccess){
@@ -35,6 +35,5 @@ int main() {
   cudaFree(arr2);
   cudaFree(arr3);
   cudaDeviceReset();
-  std::cout << std::endl;
   return 0;
 }
